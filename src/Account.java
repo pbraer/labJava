8c198f1a7febe5153886a971614212556e6f8c8e
pbraer <braer.a.s@gmail.com>
2022-11-18 21:18:17 +0700

    step 2 - finished Account & MyExceptions classes

--- src/Account.java
+++ src/Account.java
@@ -1,12 +1,53 @@
-import java.util.Date;
+/** Класс аккаунтов
+ @author p.braer
+ */
 
 public class Account {
-    public String name;
-    public Date date_birth;
-    public String email;
-    public Boolean blocked = false;
 
+    /**
+     * username - имя пользователя
+     * date_Birth - дата рождения
+     * email - почта пользователя
+     * password - пароль
+     * blocked - параметр true / false, оказывающий, заблокирован ли пользователь (false по умолчанию)
+     */
 
+    private String username;
+    private String date_Birth;
+    private String email;
+    private String password;
+    private boolean blocked;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getDate_Birth() {
+        return this.date_Birth;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public boolean getBlocked() {
+        return this.blocked;
+    }
+
+    public void setBlocked( boolean value) {
+        this.blocked = value;
+    }
+
+    Account (String username, String date_Birth, String email, String password) {
+        this.username = username;
+        this.date_Birth = date_Birth;
+        this.email = email;
+        this.password = password;
+        this.blocked = false;
+    }
 
 }
-
---
+++ src/MyExceptions.java
@@ -0,0 +1,14 @@
+/**
+ * Класс собственных обрабатываемых ошибок
+ *
+ * @author p.braer
+ */
+
+public class MyExceptions extends Exception {
+
+    public MyExceptions(String message) {
+
+        super(message);
+    }
+}
+
